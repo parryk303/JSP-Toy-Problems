60e5dc14e95d85dc195ea18c574afb1ea78d333b
Kyle Parry <kyle.parry.303@gmail.com>
2021-04-27 10:21:23 -0600

    Toy 2 complete

---
+++ Reverse Integer.js
@@ -0,0 +1,36 @@
+/*
+LINK: https://leetcode.com/problems/reverse-integer/
+
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+Input: x = 123
+Output: 321
+
+Example 2:
+Input: x = -123
+Output: -321
+
+Example 3:
+Input: x = 120
+Output: 21
+
+Example 4:
+Input: x = 0
+Output: 0
+
+Constraints: -231 <= x <= 231 - 1
+*/
+
+const reverse = (x) => {
+  const limit = 2147483648;
+  const k = x < 0 ? -1 : 1;
+  const n = Number(String(Math.abs(x)).split('').reverse().join(''));
+  return n > limit ? 0 : n * k;
+};
+
+console.log(reverse(123)) // 321
+console.log(reverse(-123)) // -321
+console.log(reverse(120)) // 21
+console.log(reverse(0)) // 0
