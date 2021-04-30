f60dcd6f5bca8b3dfade0372ff1a6d2c0152fb67
Kyle Parry <kyle.parry.303@gmail.com>
2021-04-30 10:03:05 -0600

    Toy 5 complete

---
+++ Valid Parentheses.js
@@ -0,0 +1,49 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Example 4:
+Input: s = "([)]"
+Output: false
+
+Example 5:
+Input: s = "{[]}"
+Output: true
+
+Constraints:
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+*/
+const isValid = ([...s]) => {
+  return s.reduce((uptoPrevChar, thisChar) => {
+    ((thisChar === '(' && uptoPrevChar++ || thisChar === ')' && uptoPrevChar--)) &&
+      ((thisChar === '{' && uptoPrevChar++ || thisChar === '}' && uptoPrevChar--)) &&
+      ((thisChar === '[' && uptoPrevChar++ || thisChar === ']' && uptoPrevChar--));
+
+    return uptoPrevChar;
+  }, 0) === 0
+}
+
+console.log(
+  isValid('()'), //true
+  isValid('()[]{}'), //false
+  isValid('(]'), //false
+  isValid('([)]'), //false
+  isValid('{[]}'), //true
+)
